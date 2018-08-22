4bd0879f6cd79a3e1103496d0bbbdf7e11470944
Neha Chopade <nehachopade22@gmail.com>
2018-08-22 00:26:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init() 
+{
+    let tracker=0;
+    const body= document.selector('body')
+    body.addEventListener('keydown', (event) => {
+    if (event.which===code[tracker])
+    {tracker++}
+    else 
+      {console.log ('You've messed up')
+      tracker=0;}
+  
+    }
 
+  )
 }
+init()
