d1eca3066bc4f6c545b539313f32fddc89691186
Neha Chopade <nehachopade22@gmail.com>
2018-08-22 00:29:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() 
 {
     let tracker=0;
-    const body= document.selector('body')
+    const body= document.querySelector('body')
     body.addEventListener('keydown', (event) => 
     {
         if (event.which===code[tracker])
