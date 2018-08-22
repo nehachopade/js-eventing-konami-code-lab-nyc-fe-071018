ea66775f917ad2efb0b2c4a855a71a000ce0e5f0
Neha Chopade <nehachopade22@gmail.com>
2018-08-22 00:54:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,17 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let tracker=0
-function onKeyDownHandler() 
+
+function onKeyDownHandler(event) 
 {
-    let tracker=0;
-    const body= document.querySelector('body')
+    const key=parseInt(event.detail || event.which)
     const paragraph=document.createElement('p')
     if(tracker===code.length-1){
-          
           event.preventDefault()
           body.append('Great Job', paragraph)
           return
         }
-        if (event.which===code[tracker])
+        if (key===code[tracker])
           { console.log ("Success")
             if(tracker===code.length-1)
             { alert('You found it')}
@@ -20,11 +19,10 @@ function onKeyDownHandler()
         else
           { console.log ("You've messed up")
             tracker=0;}
-    
    
 }
 function init ()
-{
+{  const body= document.querySelector('body')
    body.addEventListener('keydown', onKeyDownHandler => {})
 }
 
